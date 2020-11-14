dc97fc286bb82572a1391ee103b4466b5fbbfe33
Rohan Parmar <rohan_parmar@sfu.ca>
2020-11-14 22:30:50 +0530

    Third layout iteration

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+
+msg = """Choose the following options:
+\n\nQ: Quit
+\nO: Open Image
+\nS: Save Current Image
+\nR: Reload Original Image
+\n\n --Basic Mode--
+\n1. Invert
+\n2. Flip Horizontal
+\n3. Flip Vertical
+\n4. Switch to Intermediate Functions
+\n5. Switch to Advanced Functions
+\n\nEnter your choice (Q/O/S/R or 1-5):\n"""
+
+print(msg)
